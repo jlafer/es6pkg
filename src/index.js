3a25c64370bffd0a9485dfee579679997b559d19
jlafer <jlafer@twilio.com>
2020-07-14 13:56:17 -0700

    added setEncoding to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import {checkEnvVariable, corsResponse, sendCorsResponse} from './fns';
 import {
   setBaseUrl, addBasicCredentials, addBearerToken, addFlexToken, addTokenAsData,
-  addHeader, makeConfig, callApi
+  addHeader, setEncoding, makeConfig, callApi
 } from './network'
 export {
   checkEnvVariable, corsResponse, sendCorsResponse,
   setBaseUrl, addBasicCredentials, addBearerToken, addFlexToken, addTokenAsData,
-  addHeader, makeConfig, callApi
+  addHeader, setEncoding, makeConfig, callApi
 };
